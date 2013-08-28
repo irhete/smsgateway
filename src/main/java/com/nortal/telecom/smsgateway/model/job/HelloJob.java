6660f9b3e78d0010fe5a88e7b762b09b7dca3841
unknown <irene@ml642.webmedia.int>
2013-08-28 10:28:49 +0300

    scheduler

---
+++ src/main/java/com/nortal/telecom/smsgateway/model/job/HelloJob.java
@@ -0,0 +1,13 @@
+package com.nortal.telecom.smsgateway.model.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class HelloJob implements Job {
+
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		System.out.println("Hello! HelloJob is executing.");
+	}
+
+}
